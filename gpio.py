8e1de820ac7ac431e76c2ca09de2ffb7b0a64523
Tom Oldbury <toldbury@gmail.com>
2020-06-15 20:38:20 +0100

    Hacks

--- gpio.py
+++ gpio.py
@@ -2,7 +2,11 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)  
 
 GPIO.setup(12, GPIO.OUT)  
+GPIO.setup(13, GPIO.IN)  
 
-while True:
+def cb(*args):
     GPIO.output(12, 1)
     GPIO.output(12, 0)
+
+GPIO.add_event_detect(13, GPIO.RISING, callback=cb, bouncetime=0)  
+
